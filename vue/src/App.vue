861d1244d4d44fb6bceb0cb44fa35da820b479c1
Bngan1101 <nganbnbtb01240@gmail.com>
2026-03-31 09:30:46 +0700

    vue

---
+++ vue/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+// No global logic needed yet
+</script>
+
+<style>
+/* Global resets or main styles are in style.css */
+</style>
